33d2975b3a9918941fe5418740981bb16526ecd0
dustein <steindu@gmail.com>
2023-03-13 15:44:04 -0300

    iniciando scripts

---
+++ script.js
@@ -0,0 +1,10 @@
+
+const l250 = document.querySelector("#l250").value;
+const l250color = document.querySelector("@l250");
+
+
+function calcular() {
+  console.log(l250)
+  l250color.innerHTML = "coisa"
+  return l250
+}
