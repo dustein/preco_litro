7a437906693cbf86989c55018adb9ea3dccd6b40
dustein <steindu@gmail.com>
2023-04-25 16:31:39 -0300

    estilizando input

--- script.js
+++ script.js
@@ -11,20 +11,27 @@ unidades.forEach( item => {
   let valorDoInput = 0;
   item.addEventListener("click", () => {
 
-    valorDoInput = Number(window.prompt(`Preço da unidade ${item.dataset.key}ml :`, "0.00"))
+    valorDoInput = Number(window.prompt(`Preço da unidade ${item.dataset.key}ml :`, "0.00"));
+
     precoLitro = valorDoInput * 1000 / item.dataset.key
     
-    console.log(valorDoInput, precoLitro, item.dataset.key)
+    item.style.border = "solid 0.4rem rgb(23, 239, 95)";
+    const textoInput = item.querySelector("#input")
+    textoInput.innerText = `R$ ${valorDoInput}`
+    
+
+    console.log(valorDoInput, precoLitro, item.dataset.key, item.dataset.tipo)
 
     
     //valida input
     if(typeof(valorDoInput != NaN)) {
-      precos.push([valorDoInput, Number(item.dataset.key), precoLitro])
+      precos.push([valorDoInput, Number(item.dataset.key), precoLitro, item.dataset.tipo])
     } else {
       console.log("Nao eh um numero")
     }
     //------------
 
+    nomeUnidade.style.color = "green"
   })
 })
 // -----------------
@@ -50,17 +57,22 @@ function calcular() {
     console.log(precos[i])
     
   }
-  let resultado = [...maisBarato]
+  // let resultado = [...maisBarato]
   console.log("mais barato : ")
   console.log(maisBarato)
+  const tamanhoBarato = maisBarato[1];
+  const litroBarato = (maisBarato[2]).toFixed(2);
+  const tipoBarato = maisBarato[3];
   
   
-  console.log(`A unidade de ${maisBarato[1]}ml é a mais barata, saindo a R$${maisBarato[2]} por litro`)
+  console.log(`A ${tipoBarato} de ${tamanhoBarato}ml é a mais barata, saindo a R$${litroBarato} o litro`)
 
   const resultModal = document.querySelector("#result-modal");
+  const resultTipo = document.querySelector("#melhorTipo")
   const resulUnidade = document.querySelector("#melhorUnidade");
   const resultPrecoLitro = document.querySelector("#melhorPrecoLitro");
-  resulUnidade.innerText = maisBarato[1];
-  resultPrecoLitro.innerText = maisBarato[2];
+  resultTipo.innerText = tipoBarato;
+  resulUnidade.innerText = tamanhoBarato;
+  resultPrecoLitro.innerText = litroBarato;
   resultModal.style.display = "block"
 }
